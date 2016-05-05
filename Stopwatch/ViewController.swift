8eebca12e3f2658a714e77e600b4c30a1a3c8d66
Fabian Vergara <vergarf@gmail.com>
2016-05-04 21:37:28 -0400

    Responsive layout Toolbar and nav bar implemented Basic stopwatch with three main functions: - Stop (resets the counter) - Play (starts from 0 and re start a previous paused timer - Pause

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -10,9 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var time:Int = 0
+    private var timer = NSTimer()
+    @IBOutlet var timerLabel: UILabel!
+    
+    
+    
+    private func setTimerLabel(timerLabelToBeSet: Int){
+        timerLabel.text = String(timerLabelToBeSet)
+        
+    }
+    
+    
+    //Stop resets the timer
+    @IBAction func stopButton(sender: AnyObject) {
+        print("stop")
+        self.time = -1
+        print(self.time)
+        
+    }
+    @IBAction func playButton(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTimer"), userInfo: nil, repeats: true)
+        
+        print("play")
+    }
+    
+    @IBAction func pauseButton(sender: AnyObject) {
+        print("pauuse")
+        
+        timer.invalidate()
+    }
+    
+    func updateTimer(){
+        self.time += 1
+        print("Timer updated to : \(time)")
+        setTimerLabel(self.time)
+        
+    }
+
+
+    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("tst")
     }
 
     override func didReceiveMemoryWarning() {
